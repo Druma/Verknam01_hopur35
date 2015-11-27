0d460872306334514788a69b9b34304d2a526121
axelbjornsson <axelbjornsson@gmail.com>
2015-11-27 13:22:30 +0000

    Gert beinagrind af startup menu

--- main.cpp
+++ main.cpp
@@ -1,27 +1,52 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <cstdlib>
 using namespace std;
 
-//const string FILENAME = "DATA.txt";
+void startup();
+char inputint();
+void startwork(char val);
 
 int main()
 {
-        ifstream putdata;
-        ofstream getdata;
-        putdata.open("DATA.txt", ios::app);
-    if(getdata.fail())
-    {
-        cout << "fail to open file" << endl;
-        exit();
+    startup();
+    char val = inputint();
+
+    startwork(val);
+
+	return 0;
+}
+
+void startup() {
+    cout << "Please select one of the following: " << endl;
+    cout << "---------------------------------- " << endl;
+    cout << "[1] Input a person into the list" << endl;
+    cout << "[2] Output the whole list" << endl;
+    cout << "[3] Search for a person in the list" << endl;
+    cout << "[4] Exit the program" << endl;
+}
+
+char inputint() {
+    char val;
+    cin >> val;
+    return val;
+}
+
+void startwork(char val) {
+    switch(val) {
+    case '1' :
+//        input();
+        break;
+    case '2' :
+//        output();
+        break;
+    case '3' :
+//        fsearch();
+        break;
+    case '4' :
+        break;
+    default :
+        cout << "Please input an valid option" << endl;
+        val = inputint();
+        startwork(val);
     }
-    getdata.open("")
-    if
-
-    getdata.close();
-
-
-
-        return 0;
 }
