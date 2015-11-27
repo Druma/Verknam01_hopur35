9717453d00b79907733cf560a9fc790b40f6c0b2
druma <hilara_roma_esaran_necture@hotmail.com>
2015-11-27 11:43:27 +0000

    added node class

---
+++ node.h
@@ -0,0 +1,29 @@
+# ifndef NODE_H
+# define NODE_H
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    Node(string nm, string sx, int brth, int dth = 0);
+
+private:
+    string name;
+	string sex;
+    int year_birth;
+	int year_death; // 0 = alive
+
+};
+
+// define functions
+Node::Node(string nm, string sx, int brth, int dth = 0)
+{
+    name = nm;
+	sex = sx;
+	year_birth = brth;
+	year_birth = dth;
+};
+
+
+
+#endif // node.h
