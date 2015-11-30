b38ed07c0c94656fa2da8340664135197bcd61b3
Axel Björnsson <axelbjornsson@github.com>
2015-11-30 12:45:37 +0000

    Bætt við startup.
    Sett Delete sem option

--- p1/startup.cpp
+++ p1/startup.cpp
@@ -6,7 +6,9 @@ void startup() {
     cout << "[1] Input a person/s into the list" << endl;
     cout << "[2] Output the whole list" << endl;
     cout << "[3] Search for a person in the list" << endl;
-    cout << "[4] Exit the program" << endl;
+    cout << "[4] Delete a person from the list" << endl;
+    cout << "[5] Exit the program" << endl;
+    cout << "Input: ";
 }
 
 void inputchoice(char& val) {
@@ -25,6 +27,8 @@ void startwork(char& val, int numOfSci, vector<Person>& compScientist)
         case '3' :
                 break;
         case '4' :
+                break;
+        case '5' :
                 break;
         default :
             cout << "Input is not valid!" << endl;
