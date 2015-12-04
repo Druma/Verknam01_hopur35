0af63d3214fd6ce993fa4222b7211865ce5411bf
druma <hilara_roma_esaran_necture@hotmail.com>
2015-12-04 13:07:24 +0000

    output draft

--- p1/fileManage.cpp
+++ p1/fileManage.cpp
@@ -87,3 +87,62 @@ void outputComp(){
 
 }
 */
+// basic output function
+void output()
+{
+	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+    QString dbName = "C:\\Users\\LINDA HA\\Desktop\\codes\\Verknam01_hopur35\\p1\\SQLiteDB\\Verknam01_hopur35.sqlite";
+	//QString dbName = "/SQLiteDB/Verknam01_hopur35.sqlite";
+	db.setDatabaseName(dbName);
+
+	bool db_ok = db.open();
+	if(db_ok){
+		QSqlQuery query(db);
+		//query.exec("SELECT * FROM Computer");
+		QString text_connection = "SELECT Person.name AS 'Person', Computer.name AS 'Computer', Computer.year_creation AS 'Year' FROM Person JOIN PersonComputer ON Person.ID = PersonComputer.id_person JOIN Computer ON PersonComputer.id_computer = Computer.ID;";
+		QString text_person = "SELECT name, sex, year_birth AS 'birth', year_death AS 'death' FROM Person;";
+		QString text_computer = "SELECT name, year_creation AS 'creation', was_built AS 'built', type FROM Computer;";
+		
+		query.exec(text_connection);
+		if(query.lastError().isValid())
+			cout << query.lastError().text().toStdString() << endl;
+		else
+		{
+			if(undefined_temp = "Connection")
+			{
+				// text_connection
+				cout << "Person, Computer, Year" << endl;
+				cout << "----------------------" << endl;
+				while(query.next())
+				{
+					cout << query.value("Person").toString().toStdString() << " | " << query.value("Computer").toString().toStdString() << " | " << query.value("Year").toString().toStdString() << endl;
+				}
+			} else if(undefined_temp = "Person")
+			{
+				// text_person
+				cout << "Name, Sex, Year of Birth, Year of Death" << endl;
+				cout << "----------------------" << endl;
+				while(query.next())
+				{
+					cout << query.value("name").toString().toStdString() << " | " << query.value("sex").toString().toStdString() << " | " << query.value("birth").toString().toStdString()  << " | " << query.value("death").toString().toStdString() << endl;
+				}
+			} else if(undefined_temp = "computer")
+			{
+				// text_computer
+				cout << "Name, Creation Time, Type, Was Built(?)" << endl;
+				cout << "----------------------" << endl;
+				while(query.next())
+				{
+					cout << query.value("name").toString().toStdString() << " | " << query.value("creation").toString().toStdString() << " | " << query.value("type").toString().toStdString()  << " | ";
+					if(query.value("death").toString().toStdString() == "0")
+						cout << "No" << endl;
+					else
+						cout << "Yes"<< endl;
+				}
+			}
+		}
+	}
+	else
+		cout << "db.open() returned false" << endl;
+	
+}
--- p1/main.cpp
+++ p1/main.cpp
@@ -5,35 +5,6 @@ using namespace std;
 
 int main()
 {
-
-	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    QString dbName = "C:\\Users\\LINDA HA\\Desktop\\codes\\Verknam01_hopur35\\p1\\SQLiteDB\\Verknam01_hopur35.sqlite";
-	//QString dbName = "/SQLiteDB/Verknam01_hopur35.sqlite";
-	db.setDatabaseName(dbName);
-
-	bool db_ok = db.open();
-	if(db_ok){
-		QSqlQuery query(db);
-		//query.exec("SELECT * FROM Computer");
-		query.exec("SELECT Person.name AS 'Person', Computer.name AS 'Computer', Computer.year_creation AS 'Year' FROM Person JOIN PersonComputer ON Person.ID = PersonComputer.id_person JOIN Computer ON PersonComputer.id_computer = Computer.ID;");
-		if(query.lastError().isValid())
-			cout << query.lastError().text().toStdString() << endl;
-		else
-		{
-			cout << "Person, Computer, Year" << endl;
-			cout << "----------------------" << endl;
-			while(query.next())
-			{
-				//cout << query.value("name").toString().toStdString() << endl;
-				cout << query.value("Person").toString().toStdString() << " | " << query.value("Computer").toString().toStdString() << " | " << query.value("Year").toString().toStdString() << endl;
-			}
-		}
-	}
-	else
-		cout << "db.open() returned false" << endl;
-	
-
-	/*
     int numOfSci = 0, numOfCom = 0;
     vector<Person> compScientist;
 	outputscie(compScientist);
@@ -43,7 +14,7 @@ int main()
         inputchoice(val);
         startwork(val, numOfSci, numOfCom);
     }while(val != EXIT);
-	*/
+	
 
     return 0;
 }
