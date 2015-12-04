2e77750c932388b7e622b83d0b21337ae3c79f76
druma <hilara_roma_esaran_necture@hotmail.com>
2015-12-04 15:40:50 +0000

    db fixes

--- p1/main.cpp
+++ p1/main.cpp
@@ -3,19 +3,31 @@
 #include <QtSql>
 using namespace std;
 QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-QString dbName = "/SQLiteDB/Verknam01_hopur35.sqlite";
+// birgitta path:
+QString dbName = "C:\\Users\\Birgitta\\Documents\\GitHub\\Verknam01_hopur35\\p1\\SQLiteDB\\Verknam01_hopur35.sqlite";
+// anna path:
+//QString dbName = "";
+// axel path:
+//QString dbName = "";
+// willard path:
+//QString dbName = "";
 int main()
 {
 	db.setDatabaseName(dbName);
     int numOfSci = 0, numOfCom = 0;
     //outputscie(compScientist);
     char val = '\0';
-	db.open();
-    do{
-        startup();
-        inputchoice(val);
-        startwork(val, numOfSci, numOfCom, db);
-    }while(val != EXIT);
+	bool db_ok = db.open();
+	if(db_ok)
+	{
+		do{
+			startup();
+			inputchoice(val);
+			startwork(val, numOfSci, numOfCom, db);
+		}while(val != EXIT);
+	}
+	else
+		cout << "db.open() returned false" << endl << "Have you changed the path variable 'dbName' in main?" << endl;
 	db.close();
 
     return 0;
