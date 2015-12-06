77e6c1504e5f2fa07ec4d49d6487f760e895b49d
Axel Björnsson <axelbjornsson@github.com>
2015-12-06 14:18:03 +0000

    Output tables in fileManager

--- p1/fileManage.cpp
+++ p1/fileManage.cpp
@@ -124,87 +124,124 @@ void outputComp(){
 // basic output function
 void output(char val, QSqlDatabase& db)
 {
-	/*
-	QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    QString dbName = "C:\\Users\\LINDA HA\\Desktop\\codes\\Verknam01_hopur35\\p1\\SQLiteDB\\Verknam01_hopur35.sqlite";
-	//QString dbName = "/SQLiteDB/Verknam01_hopur35.sqlite";
-	db.setDatabaseName(dbName);*/
-    //string undefined_temp = "defined, now!";
-	bool db_ok = db.open();
+    bool db_ok = db.open();
 	if(db_ok)
 	{
 		cout << endl;
 		QSqlQuery query(db);
-		//query.exec("SELECT * FROM Computer");
-		QString text_connection = "SELECT Person.name AS 'Person', Computer.name AS 'Computer', Computer.year_creation AS 'Year' FROM Person JOIN PersonComputer ON Person.ID = PersonComputer.id_person JOIN Computer ON PersonComputer.id_computer = Computer.ID;";
+        QString text_connection = "SELECT Person.name AS 'Person', Computer.name AS 'Computer', Computer.year_creation AS 'Year' FROM Person JOIN PersonComputer ON Person.ID = PersonComputer.id_person JOIN Computer ON PersonComputer.id_computer = Computer.ID;";
 		QString text_person = "SELECT name, sex, year_birth AS 'birth', year_death AS 'death' FROM Person;";
 		QString text_computer = "SELECT name, year_creation AS 'creation', was_built AS 'built', type FROM Computer;";
 		
 		
-		if(val == '3') // text_connection
-		{
-			query.exec(text_connection);
-			if(query.lastError().isValid())
-				cout << query.lastError().text().toStdString() << endl;
-			else
-			{
-				cout << "Person, Computer, Year" << endl;
-				cout << "----------------------" << endl;
-				while(query.next())
-				{
-					cout << query.value("Person").toString().toStdString() << " | " << query.value("Computer").toString().toStdString() << " | " << query.value("Year").toString().toStdString() << endl;
-				}
-			}
-		} else if(val == '1') // text_person
+        if(val == '1') // text_person
 		{
 			query.exec(text_person);
 			if(query.lastError().isValid())
 				cout << query.lastError().text().toStdString() << endl;
 			else
 			{
-                int width = 16;
-
-
+                int width1 = 16;
+                int width2 = 8;
                 char sep = ' ';
-                cout << left << setw(width) << setfill(sep) << "Name"           << " | "
-                     << left << setw(width/2) << setfill(sep) << "Sex"            << " | "
-                     << left << setw(width) << setfill(sep) << "Year of Birth"  << " | "
-                     << left << setw(width) << setfill(sep) << "Year of Death"  << endl;
 
-                cout << left << setw((width+3)*3 + width/2) <<setfill('-') << "" << endl;
+                cout << " " << left << setw(width1) << setfill(sep) << "Name"           << " | "
+                            << left << setw(width2) << setfill(sep) << "Sex"            << " | "
+                            << left << setw(width1) << setfill(sep) << "Year of Birth"  << " | "
+                            << left << setw(width1) << setfill(sep) << "Year of Death"  << endl;
+
+                cout << left << setw((width1+3)*3 + width2 + 1) <<setfill('-') << "" << endl;
 
 				while(query.next())
 				{
-                    cout << left  << setw(width) << setfill(sep) << query.value("name").toString().toStdString()   << " | "
-                         << left  << setw(width/2) << setfill(sep) << query.value("sex").toString().toStdString()    << " | "
-                         << right << setw(width) << setfill(sep) << query.value("birth").toString().toStdString()  << " | "
-                         << right << setw(width) << setfill(sep) << query.value("death").toString().toStdString()  << endl;
+                    cout << " " << left  << setw(width1) << setfill(sep) << query.value("name").toString().toStdString()   << " | ";
+                    if(
+                            query.value("sex").toString().toStdString()=="m"        ||
+                            query.value("sex").toString().toStdString()=="male"     ||
+                            query.value("sex").toString().toStdString()=="Male"         )
+                        cout    << left  << setw(width2) << setfill(sep) << "Male" << " | ";
+
+                    else if(
+                            query.value("sex").toString().toStdString()=="f"        ||
+                            query.value("sex").toString().toStdString()=="female"   ||
+                            query.value("sex").toString().toStdString()=="Female"       )
+                        cout    << left  << setw(width2) << setfill(sep) << "Female" << " | ";
+                    else
+                        cout    << left  << setw(width2) << setfill(sep) << "Error!" << " | ";
+
+                        cout    << right << setw(width1) << setfill(sep) << query.value("birth").toString().toStdString()  << " | "
+                                << right << setw(width1) << setfill(sep) << query.value("death").toString().toStdString()  << endl;
 
 				}
 			}
-		} else if(val == '2') // text_computer
+        }
+
+        else if(val == '2') // text_computer
 		{
 			query.exec(text_computer);
 			if(query.lastError().isValid())
 				cout << query.lastError().text().toStdString() << endl;
 			else
 			{
-				cout << "Name, Creation Time, Type, Was Built(?)" << endl;
-				cout << "----------------------" << endl;
-				while(query.next())
+                int width1 = 16;
+                int width2 = 24;
+                int width3 = 12;
+                char sep = ' ';
+
+                cout << " " << left << setw(width2) << setfill(sep) << "Name"           << " | "
+                            << left << setw(width1) << setfill(sep) << "Creation Time"  << " | "
+                            << left << setw(width3) << setfill(sep) << "Type"           << " | "
+                            << left << setw(width1) << setfill(sep) << "Was Built(?)"   << endl;
+
+                cout << left << setw((width1+3)*2 + width2 + width3) <<setfill('-') << "" << endl;
+
+                while(query.next())
 				{
-					cout << query.value("name").toString().toStdString() << " | " << query.value("creation").toString().toStdString() << " | " << query.value("type").toString().toStdString()  << " | ";
-					if(query.value("built").toString().toStdString() == "0")
-						cout << "No" << endl;
-					else
-						cout << "Yes"<< endl;
+
+                    cout << " " << left  << setw(width2) << setfill(sep) << query.value("name").toString().toStdString()      << " | "
+                                << right << setw(width1) << setfill(sep) << query.value("creation").toString().toStdString()  << " | "
+                                << left  << setw(width3) << setfill(sep) << query.value("type").toString().toStdString()      << " | ";
+                        if(query.value("built").toString().toStdString() == "1")
+                    cout        << left  << setw(width1) << setfill(sep) << "Yes"   << endl;
+                        else
+                    cout        << left  << setw(width1) << setfill(sep) << "No"    << endl;
 				}
 			}
-		} // else if text_computer END
-		cout << endl;
+        }
+
+        else if(val == '3') // text_connection
+        {
+            query.exec(text_connection);
+            if(query.lastError().isValid())
+                cout << query.lastError().text().toStdString() << endl;
+            else
+            {
+
+                int width1 = 16;
+                int width2 = 24;
+                int width3 = 8;
+
+                char sep = ' ';
+
+                cout << " " << left << setw(width1) << setfill(sep) << "Person"         << " | "
+                            << left << setw(width2) << setfill(sep) << "Computer"       << " | "
+                            << left << setw(width3) << setfill(sep) << "Year"           << endl;
+
+                cout << left << setw(width1 + width2 + width3 + 6) <<setfill('-') << "" << endl;
+
+                while(query.next())
+                {
+                    cout << " " << left << setw(width1) << setfill(sep) << query.value("Person").toString().toStdString()   << " | "
+                                << left << setw(width2) << setfill(sep) << query.value("Computer").toString().toStdString() << " | "
+                                << left << setw(width3) << setfill(sep) << query.value("Year").toString().toStdString()     << endl;
+                }
+            }
+        }
+
 	}
 	else
 		cout << "db.open() returned false" << endl;
 	db.close();
 	
 }
+
